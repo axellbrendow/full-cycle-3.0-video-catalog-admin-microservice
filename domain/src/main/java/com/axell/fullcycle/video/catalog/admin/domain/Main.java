bb18096ae5939024d7de5bbb97c9c08abbee7cba
axell-brendow <breno.axel@gmail.com>
2023-05-18 11:31:19 -0300

    Create modules for application, domain and infrastructure

---
+++ domain/src/main/java/com/axell/fullcycle/video/catalog/admin/domain/Main.java
@@ -0,0 +1,7 @@
+package com.axell.fullcycle.video.catalog.admin.domain;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ infrastructure/src/main/java/com/axell/fullcycle/video/catalog/admin/infrastructure/Main.java
@@ -0,0 +1,7 @@
+package com.axell.fullcycle.video.catalog.admin.infrastructure;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
