77ec571232f420a52777fc2844c648cf44395862
axell-brendow <breno.axel@gmail.com>
2023-05-18 15:37:02 -0300

    Add abstract method validate() in the Entity class

--- domain/src/main/java/com/axell/fullcycle/video/catalog/admin/domain/Entity.java
+++ domain/src/main/java/com/axell/fullcycle/video/catalog/admin/domain/Entity.java
@@ -2,6 +2,8 @@ package com.axell.fullcycle.video.catalog.admin.domain;
 
 import java.util.Objects;
 
+import com.axell.fullcycle.video.catalog.admin.domain.validation.ValidationHandler;
+
 public abstract class Entity<ID extends Identifier> {
     protected final ID id;
 
@@ -9,6 +11,8 @@ public abstract class Entity<ID extends Identifier> {
         Objects.requireNonNull(id, "`id` should not be null");
         this.id = id;
     }
+
+    public abstract void validate(ValidationHandler handler);
 
     public ID getId() {
         return id;
