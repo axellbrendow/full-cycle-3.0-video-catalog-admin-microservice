aa33dd81fa2d8d249a7f1bbe97de7f373559a63b
axell-brendow <breno.axel@gmail.com>
2023-05-18 15:34:56 -0300

    Add ThrowsValidationHandler which throws exceptions

---
+++ domain/src/main/java/com/axell/fullcycle/video/catalog/admin/domain/validation/handler/ThrowsValidationHandler.java
@@ -0,0 +1,35 @@
+package com.axell.fullcycle.video.catalog.admin.domain.validation.handler;
+
+import java.util.List;
+
+import com.axell.fullcycle.video.catalog.admin.domain.exceptions.DomainException;
+import com.axell.fullcycle.video.catalog.admin.domain.validation.Error;
+import com.axell.fullcycle.video.catalog.admin.domain.validation.ValidationHandler;
+
+public class ThrowsValidationHandler implements ValidationHandler {
+    @Override
+    public ValidationHandler append(final Error error) {
+        throw DomainException.with(List.of(error));
+    }
+
+    @Override
+    public ValidationHandler append(final ValidationHandler handler) {
+        throw DomainException.with(handler.getErrors());
+    }
+
+    @Override
+    public ValidationHandler validate(final Validation validation) {
+        try {
+            validation.validate();
+        } catch (final Exception e) {
+            throw DomainException.with(List.of(new Error(e.getMessage())));
+        }
+
+        return this;
+    }
+
+    @Override
+    public List<Error> getErrors() {
+        return List.of();
+    }
+}
