b31a568609a631748a7a43ee8be2867ab7ef24bd
axell-brendow <breno.axel@gmail.com>
2023-05-18 19:22:03 -0300

    Add more tests to CreateCategoryUseCase

--- application/src/test/java/com/axell/fullcycle/video/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/axell/fullcycle/video/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
@@ -4,12 +4,24 @@ import java.util.Objects;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.AdditionalAnswers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.axell.fullcycle.video.catalog.admin.domain.category.CategoryRepository;
+import com.axell.fullcycle.video.catalog.admin.domain.exceptions.DomainException;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryRepository repository;
+
     @Test
     public void givenAValidCommand_whenCallCreateCategory_shouldReturnCategoryId() {
         final var expectedName = "Movies";
@@ -18,11 +30,8 @@ public class CreateCategoryUseCaseTest {
 
         final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
 
-        final var repository = Mockito.mock(CategoryRepository.class);
         Mockito.when(repository.create(Mockito.any()))
                 .thenAnswer(AdditionalAnswers.returnsFirstArg());
-
-        final var useCase = new DefaultCreateCategoryUseCase(repository);
 
         final var output = useCase.execute(command);
 
@@ -38,6 +47,77 @@ public class CreateCategoryUseCaseTest {
                             && Objects.nonNull(category.getCreatedAt())
                             && Objects.nonNull(category.getUpdatedAt())
                             && Objects.isNull(category.getDeletedAt());
+                }));
+    }
+
+    @Test
+    public void givenAInvalidName_whenCallCreateCategory_shouldThrowDomainException() {
+        final String expectedName = null;
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "`name` should not be null";
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        final var exception = Assertions.assertThrows(DomainException.class, () -> useCase.execute(command));
+
+        Assertions.assertEquals(expectedErrorMessage, exception.getMessage());
+        Mockito.verify(repository, Mockito.times(0)).create(Mockito.any());
+    }
+
+    @Test
+    public void givenAValidCommandWithInactiveCategory_whenCallCreateCategory_shouldReturnInactiveCategoryId() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = false;
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(repository.create(Mockito.any()))
+                .thenAnswer(AdditionalAnswers.returnsFirstArg());
+
+        final var output = useCase.execute(command);
+
+        Assertions.assertNotNull(output);
+        Assertions.assertNotNull(output.id());
+
+        Mockito.verify(repository, Mockito.times(1))
+                .create(Mockito.argThat(category -> {
+                    return Objects.equals(expectedName, category.getName())
+                            && Objects.equals(expectedDescription, category.getDescription())
+                            && Objects.equals(expectedIsActive, category.isActive())
+                            && Objects.nonNull(category.getId())
+                            && Objects.nonNull(category.getCreatedAt())
+                            && Objects.nonNull(category.getUpdatedAt())
+                            && Objects.nonNull(category.getDeletedAt());
+                }));
+    }
+
+    @Test
+    public void givenAValidCommand_whenRepositoryThrowsException_shouldThrowException() {
+        final var expectedName = "Movies";
+        final var expectedDescription = "Most watched category";
+        final var expectedIsActive = false;
+        final var expectedErrorMessage = "Repository error";
+
+        final var command = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(repository.create(Mockito.any()))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var exception = Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(command));
+
+        Assertions.assertEquals(expectedErrorMessage, exception.getMessage());
+
+        Mockito.verify(repository, Mockito.times(1))
+                .create(Mockito.argThat(category -> {
+                    return Objects.equals(expectedName, category.getName())
+                            && Objects.equals(expectedDescription, category.getDescription())
+                            && Objects.equals(expectedIsActive, category.isActive())
+                            && Objects.nonNull(category.getId())
+                            && Objects.nonNull(category.getCreatedAt())
+                            && Objects.nonNull(category.getUpdatedAt())
+                            && Objects.nonNull(category.getDeletedAt());
                 }));
     }
 }
