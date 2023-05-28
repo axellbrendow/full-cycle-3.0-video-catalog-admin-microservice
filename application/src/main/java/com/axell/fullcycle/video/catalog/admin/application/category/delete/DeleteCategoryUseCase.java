01314b60b71b2c4d441b5da60d44bd5c252c6d0e
axell-brendow <breno.axel@gmail.com>
2023-05-28 00:03:09 -0300

    Implement DeleteCategoryUseCase

---
+++ application/src/main/java/com/axell/fullcycle/video/catalog/admin/application/category/delete/DeleteCategoryUseCase.java
@@ -0,0 +1,6 @@
+package com.axell.fullcycle.video.catalog.admin.application.category.delete;
+
+import com.axell.fullcycle.video.catalog.admin.application.InputOnlyUseCase;
+
+public abstract class DeleteCategoryUseCase extends InputOnlyUseCase<String> {
+}
