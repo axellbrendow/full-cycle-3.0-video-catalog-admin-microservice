325c5cbd66cded89cdde69bc62ffecb4f972a545
axell-brendow <breno.axel@gmail.com>
2023-05-28 03:21:38 -0300

    Add Spring Data JPA and configure properties

--- infrastructure/src/test/java/com/axell/fullcycle/video/catalog/admin/infrastructure/MainTest.java
+++ infrastructure/src/test/java/com/axell/fullcycle/video/catalog/admin/infrastructure/MainTest.java
@@ -1,12 +1,4 @@
 package com.axell.fullcycle.video.catalog.admin.infrastructure;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 public class MainTest {
-    @Test
-    public void testMain() {
-        Assertions.assertNotNull(new Main());
-        Main.main(new String[] {});
-    }
 }
